8e4ee95a302d8e257f515d031acdc395c0e19eb4
n33vb4k <bakshi.neev@gmail.com>
2024-06-06 23:40:15 +0100

    setup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from telethon import TelegramClient, events
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+
+client = TelegramClient('copier', api_id, api_hash)
