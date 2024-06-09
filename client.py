fbdb818eda2b7a7ebe89d26eecafa23b55e2e390
n33vb4k <bakshi.neev@gmail.com>
2024-06-10 00:46:02 +0100

    can now interpret the buy/sell signals

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import MetaTrader5 as mt5
 import os
 import itertools
 from dotenv import load_dotenv
+from trader import *
 
 load_dotenv()
 
@@ -15,26 +16,43 @@ action_queue = []
 
 """
 To Do:
-- Add function to place trades
-- Add function to move stop loss
-- Add function to close trades
-- Add these to a queue to be executed in order
-- intergrate NLP model to understand messages better
+- Add orders to a queue to be executed in order
+- Only execute by/sell orders if it is +/- 10 pips from entry price
+- Add function to read move sl messages and close trade messages
 - Add function to log trades and actions with date and time
 
 """
 
+premium_members = 1001268664484
+testing_group = 4255421846
+
 async def main():
+    # Start client
     await client.start()
     print("Client Started and Connected")
 
-    @client.on(events.NewMessage(chats=1001268664484))
+    # Initialize MT5
+    if not mt5.initialize():
+        print("Failed to initialize")
+        return
+    print("MT5 Initialized")
+
+    # Login to MT5
+    if not mt5.login(int(os.getenv("mt5_login")), os.getenv("mt5_password"), server="MetaQuotes-Demo"):
+        print("Failed to login")
+        return
+    print("Logged in to MT5")
+
+    # Event handler for new messages
+    @client.on(events.NewMessage(chats=testing_group))
     async def handler(event):
         text = event.text.lower()
         print(text)
         if 'buy' in text or 'sell' in text:
+            action, symbol, entry_price, sl, tps = process_trading_signal(text)
             #function to place trades
-            await client.send_message('me', event.text)
+            await client.send_message('me', f"Action: {action}, Symbol: {symbol}, Entry Price: {entry_price}, SL: {sl}, TPs: {tps}")
+
         elif "stoploss" and "entry" in text:
             #function to move sl to entry
             await client.send_message('me', event.text)
@@ -51,7 +69,3 @@ async def main():
 with client:
     client.loop.run_until_complete(main())
 
-def message_simplify(text):
-    return ''.join(
-        ch for ch, _ in itertools.groupby(text)
-    )
